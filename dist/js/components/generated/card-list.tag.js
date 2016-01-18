a36938d330a168c9ddc365304ddb30a4fb3e51bb
Prateek  Bhatnagar <prateek89born@gmail.com>
2016-01-18 21:37:58 +0530

    Adding dist to sourcecode for gh-pages

---
+++ dist/js/components/generated/card-list.tag.js
@@ -0,0 +1,13 @@
+riot.tag2('card-list', '<div class="card-list"> <loader if="{!isDataLoaded}"></loader> <div if="{isDataLoaded}" class="card-list__card" each="{projects}" data-filter="{name.toLowerCase()} {imgAlt.toLowerCase()} {url.topLowerCase()}"> <div class="card-list__image"> <img riot-src="{imgUrl}" alt="{imgAlt}"> </div> <h2 class="card-list__name"> {name} </h2> <div class="card-list__url"> {url} </div> <a href="#/details/{id}" class="card-list__details"> View Details </a> </div>', '', '', function(opts) {
+        var _self=this;
+        this.isDataLoaded=false;
+        this.projects=[];
+        this.on("mount", function() {
+            getProjectData().then(function(e) {
+                _self.update({
+                    isDataLoaded:true,
+                    projects:e.projects
+                });
+            });
+        });
+}, '{ }');
