a1c6a340297a725953fcfa1eefef64130672320e
Kazzkiq <claudiohw@gmail.com>
2016-01-18 15:49:52 -0200

    Update 2016-01-18T17:49:49.565Z

--- js/components/generated/top-bar.tag.js
+++ js/components/generated/top-bar.tag.js
@@ -1,9 +1,17 @@
-riot.tag2('top-bar', '<header class="top-bar" name="mainTopBar"> <div class="container"> <div class="top-bar__mobile-menu"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearch" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> <h1>{opts.slogan}</h1> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header> <header class="top-bar state--compact" name="compactTopBar"> <div class="container"> <div class="top-bar__mobile-menu"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearchCompact" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header>', '', '', function(opts) {
+riot.tag2('top-bar', '<div class="top-bar__mobile-menu" name="mobileMenu"> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearch" placeholder="Search" onkeyup="{filterCards}"> </div> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> <header class="top-bar" name="mainTopBar"> <div class="container"> <div class="top-bar__mobile-trigger" onclick="{toggleMobileMenu}"> <svg class="svg-icon icon-menu" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> <svg class="svg-icon icon-close" role="img" title="Preview Mode" width="18"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-close"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearch" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> <h1>{opts.slogan}</h1> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header> <header class="top-bar state--compact" name="compactTopBar"> <div class="container"> <div class="top-bar__mobile-trigger" onclick="{toggleMobileMenu}"> <svg class="svg-icon icon-menu" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> <svg class="svg-icon icon-close" role="img" title="Preview Mode" width="18"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-close"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearchCompact" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header>', '', '', function(opts) {
         var self = this;
         var visibleClass = 'state--visible';
         var styleElement = document.createElement('STYLE');
 
         self.on('mount', function() {
+
+            self.isMenuOpen = false;
+
+            self.cards = document.querySelector(opts.cardSelector);
+
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+
             document.addEventListener('scroll', function() {
 
                 if(window.pageYOffset < window.innerHeight) {
@@ -49,7 +57,6 @@ riot.tag2('top-bar', '<header class="top-bar" name="mainTopBar"> <div class="con
         }.bind(this)
 
         this.filterCards = function(e) {
-
             var value = e.target.value;
 
             if(e.target.name == 'inputSearch') {
@@ -69,5 +76,54 @@ riot.tag2('top-bar', '<header class="top-bar" name="mainTopBar"> <div class="con
             }
 
             return true;
+        }.bind(this)
+
+        this.toggleMobileMenu = function() {
+            if(self.isMenuOpen) {
+                self.isMenuOpen = !self.isMenuOpen;
+                self.closeMobileMenu();
+            }else {
+                self.isMenuOpen = !self.isMenuOpen;
+                self.openMobileMenu();
+            }
+        }.bind(this)
+
+        this.openMobileMenu = function() {
+
+            self.clearMobileMenuClasses();
+
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.remove('active');
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.add('active');
+
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.remove('active');
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.add('active');
+
+            self.cards.classList.add('animate','animate-slide-right');
+            self.mainTopBar.classList.add('animate','animate-slide-right');
+            self.compactTopBar.childNodes[1].classList.add('animate','animate-slide-right');
+            self.mobileMenu.classList.add('animate', 'animate-fade-in-up');
+        }.bind(this)
+
+        this.closeMobileMenu = function() {
+
+            self.clearMobileMenuClasses();
+
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.remove('active');
+
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.remove('active');
+
+            self.cards.classList.add('animate','animate-slide-left');
+            self.mainTopBar.classList.add('animate','animate-slide-left');
+            self.compactTopBar.childNodes[1].classList.add('animate','animate-slide-left');
+            self.mobileMenu.classList.add('animate', 'animate-fade-out-z');
+        }.bind(this)
+
+        this.clearMobileMenuClasses = function() {
+            self.cards.classList.remove('animate', 'animate-slide-left', 'animate-slide-right');
+            self.mainTopBar.classList.remove('animate','animate-slide-left', 'animate-slide-right');
+            self.compactTopBar.childNodes[1].classList.remove('animate','animate-slide-left', 'animate-slide-right');
+            self.mobileMenu.classList.remove('animate', 'animate-fade-out-z', 'animate-fade-in-up');
         }.bind(this)
 }, '{ }');
