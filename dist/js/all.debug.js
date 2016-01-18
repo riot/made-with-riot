6301a3a694a40f6900371eeb6e3bc3f48a18bdd7
Kazzkiq <claudiohw@gmail.com>
2016-01-18 16:15:15 -0200

    Removing dist folder

--- dist/js/all.debug.js
+++ dist/js/all.debug.js
@@ -1,201 +1,3 @@
-(function(root) {
-
-	// Store setTimeout reference so promise-polyfill will be unaffected by
-	// other code modifying setTimeout (like sinon.useFakeTimers())
-	var setTimeoutFunc = setTimeout;
-
-	// Use polyfill for setImmediate for performance gains
-	var asap = (typeof setImmediate === 'function' && setImmediate) ||
-		function(fn) { setTimeoutFunc(fn, 1); };
-
-	// Polyfill for Function.prototype.bind
-	function bind(fn, thisArg) {
-		return function() {
-			fn.apply(thisArg, arguments);
-		}
-	}
-
-	var isArray = Array.isArray || function(value) { return Object.prototype.toString.call(value) === "[object Array]" };
-
-	function Promise(fn) {
-		if (typeof this !== 'object') throw new TypeError('Promises must be constructed via new');
-		if (typeof fn !== 'function') throw new TypeError('not a function');
-		this._state = null;
-		this._value = null;
-		this._deferreds = []
-
-		doResolve(fn, bind(resolve, this), bind(reject, this))
-	}
-
-	function handle(deferred) {
-		var me = this;
-		if (this._state === null) {
-			this._deferreds.push(deferred);
-			return
-		}
-		asap(function() {
-			var cb = me._state ? deferred.onFulfilled : deferred.onRejected
-			if (cb === null) {
-				(me._state ? deferred.resolve : deferred.reject)(me._value);
-				return;
-			}
-			var ret;
-			try {
-				ret = cb(me._value);
-			}
-			catch (e) {
-				deferred.reject(e);
-				return;
-			}
-			deferred.resolve(ret);
-		})
-	}
-
-	function resolve(newValue) {
-		try { //Promise Resolution Procedure: https://github.com/promises-aplus/promises-spec#the-promise-resolution-procedure
-			if (newValue === this) throw new TypeError('A promise cannot be resolved with itself.');
-			if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
-				var then = newValue.then;
-				if (typeof then === 'function') {
-					doResolve(bind(then, newValue), bind(resolve, this), bind(reject, this));
-					return;
-				}
-			}
-			this._state = true;
-			this._value = newValue;
-			finale.call(this);
-		} catch (e) { reject.call(this, e); }
-	}
-
-	function reject(newValue) {
-		this._state = false;
-		this._value = newValue;
-		finale.call(this);
-	}
-
-	function finale() {
-		for (var i = 0, len = this._deferreds.length; i < len; i++) {
-			handle.call(this, this._deferreds[i]);
-		}
-		this._deferreds = null;
-	}
-
-	function Handler(onFulfilled, onRejected, resolve, reject){
-		this.onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : null;
-		this.onRejected = typeof onRejected === 'function' ? onRejected : null;
-		this.resolve = resolve;
-		this.reject = reject;
-	}
-
-	/**
-	 * Take a potentially misbehaving resolver function and make sure
-	 * onFulfilled and onRejected are only called once.
-	 *
-	 * Makes no guarantees about asynchrony.
-	 */
-	function doResolve(fn, onFulfilled, onRejected) {
-		var done = false;
-		try {
-			fn(function (value) {
-				if (done) return;
-				done = true;
-				onFulfilled(value);
-			}, function (reason) {
-				if (done) return;
-				done = true;
-				onRejected(reason);
-			})
-		} catch (ex) {
-			if (done) return;
-			done = true;
-			onRejected(ex);
-		}
-	}
-
-	Promise.prototype['catch'] = function (onRejected) {
-		return this.then(null, onRejected);
-	};
-
-	Promise.prototype.then = function(onFulfilled, onRejected) {
-		var me = this;
-		return new Promise(function(resolve, reject) {
-			handle.call(me, new Handler(onFulfilled, onRejected, resolve, reject));
-		})
-	};
-
-	Promise.all = function () {
-		var args = Array.prototype.slice.call(arguments.length === 1 && isArray(arguments[0]) ? arguments[0] : arguments);
-
-		return new Promise(function (resolve, reject) {
-			if (args.length === 0) return resolve([]);
-			var remaining = args.length;
-			function res(i, val) {
-				try {
-					if (val && (typeof val === 'object' || typeof val === 'function')) {
-						var then = val.then;
-						if (typeof then === 'function') {
-							then.call(val, function (val) { res(i, val) }, reject);
-							return;
-						}
-					}
-					args[i] = val;
-					if (--remaining === 0) {
-						resolve(args);
-					}
-				} catch (ex) {
-					reject(ex);
-				}
-			}
-			for (var i = 0; i < args.length; i++) {
-				res(i, args[i]);
-			}
-		});
-	};
-
-	Promise.resolve = function (value) {
-		if (value && typeof value === 'object' && value.constructor === Promise) {
-			return value;
-		}
-
-		return new Promise(function (resolve) {
-			resolve(value);
-		});
-	};
-
-	Promise.reject = function (value) {
-		return new Promise(function (resolve, reject) {
-			reject(value);
-		});
-	};
-
-	Promise.race = function (values) {
-		return new Promise(function (resolve, reject) {
-			for(var i = 0, len = values.length; i < len; i++) {
-				values[i].then(resolve, reject);
-			}
-		});
-	};
-
-	/**
-	 * Set the immediate function to execute callbacks
-	 * @param fn {function} Function to execute
-	 * @private
-	 */
-	Promise._setImmediateFn = function _setImmediateFn(fn) {
-		asap = fn;
-	};
-
-	if (typeof module !== 'undefined' && module.exports) {
-		module.exports = Promise;
-	} else if (!root.Promise) {
-		root.Promise = Promise;
-	}
-
-})(this);
-
-
-//-----
-
 var GoogleAnalytics = (function(){
 
     function GoogleAnalytics() {
@@ -2709,12 +2511,20 @@ riot.tag2('right-drawer', '<div class="phantom" if="{state===\'opened\'}" onclic
 
 //-----
 
-riot.tag2('top-bar', '<header class="top-bar" name="mainTopBar"> <div class="container"> <div class="top-bar__mobile-menu"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearch" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> <h1>{opts.slogan}</h1> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header> <header class="top-bar state--compact" name="compactTopBar"> <div class="container"> <div class="top-bar__mobile-menu"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearchCompact" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header>', '', '', function(opts) {
+riot.tag2('top-bar', '<div class="top-bar__mobile-menu" name="mobileMenu"> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearch" placeholder="Search" onkeyup="{filterCards}"> </div> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> <header class="top-bar" name="mainTopBar"> <div class="container"> <div class="top-bar__mobile-trigger" onclick="{toggleMobileMenu}"> <svg class="svg-icon icon-menu" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> <svg class="svg-icon icon-close" role="img" title="Preview Mode" width="18"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-close"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearch" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> <h1>{opts.slogan}</h1> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header> <header class="top-bar state--compact" name="compactTopBar"> <div class="container"> <div class="top-bar__mobile-trigger" onclick="{toggleMobileMenu}"> <svg class="svg-icon icon-menu" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-menu"></use> </svg> <svg class="svg-icon icon-close" role="img" title="Preview Mode" width="18"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-close"></use> </svg> </div> <div class="top-bar__search-bar"> <svg class="svg-icon" role="img" title="Preview Mode" width="22"> <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="img/all.min.svg#icon-search"></use> </svg> <input type="text" name="inputSearchCompact" placeholder="Search" onkeyup="{filterCards}"> </div> <div class="top-bar__landing"> <img riot-src="{opts.logoUrl}" alt="{opts.logoAlt}" width="147"> </div> <div class="top-bar__right-menu"> <a href="#" each="{opts.menu}" class="top-bar__link {state--attention: isImportant}"> {text} </a> </div> </div> </header>', '', '', function(opts) {
         var self = this;
         var visibleClass = 'state--visible';
         var styleElement = document.createElement('STYLE');
 
         self.on('mount', function() {
+
+            self.isMenuOpen = false;
+
+            self.cards = document.querySelector(opts.cardSelector);
+
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+
             document.addEventListener('scroll', function() {
 
                 if(window.pageYOffset < window.innerHeight) {
@@ -2760,7 +2570,6 @@ riot.tag2('top-bar', '<header class="top-bar" name="mainTopBar"> <div class="con
         }.bind(this)
 
         this.filterCards = function(e) {
-
             var value = e.target.value;
 
             if(e.target.name == 'inputSearch') {
@@ -2780,5 +2589,54 @@ riot.tag2('top-bar', '<header class="top-bar" name="mainTopBar"> <div class="con
             }
 
             return true;
+        }.bind(this)
+
+        this.toggleMobileMenu = function() {
+            if(self.isMenuOpen) {
+                self.isMenuOpen = !self.isMenuOpen;
+                self.closeMobileMenu();
+            }else {
+                self.isMenuOpen = !self.isMenuOpen;
+                self.openMobileMenu();
+            }
+        }.bind(this)
+
+        this.openMobileMenu = function() {
+
+            self.clearMobileMenuClasses();
+
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.remove('active');
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.add('active');
+
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.remove('active');
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.add('active');
+
+            self.cards.classList.add('animate','animate-slide-right');
+            self.mainTopBar.classList.add('animate','animate-slide-right');
+            self.compactTopBar.childNodes[1].classList.add('animate','animate-slide-right');
+            self.mobileMenu.classList.add('animate', 'animate-fade-in-up');
+        }.bind(this)
+
+        this.closeMobileMenu = function() {
+
+            self.clearMobileMenuClasses();
+
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+            self.mainTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.remove('active');
+
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-menu').classList.add('active');
+            self.compactTopBar.querySelector('.top-bar__mobile-trigger .icon-close').classList.remove('active');
+
+            self.cards.classList.add('animate','animate-slide-left');
+            self.mainTopBar.classList.add('animate','animate-slide-left');
+            self.compactTopBar.childNodes[1].classList.add('animate','animate-slide-left');
+            self.mobileMenu.classList.add('animate', 'animate-fade-out-z');
+        }.bind(this)
+
+        this.clearMobileMenuClasses = function() {
+            self.cards.classList.remove('animate', 'animate-slide-left', 'animate-slide-right');
+            self.mainTopBar.classList.remove('animate','animate-slide-left', 'animate-slide-right');
+            self.compactTopBar.childNodes[1].classList.remove('animate','animate-slide-left', 'animate-slide-right');
+            self.mobileMenu.classList.remove('animate', 'animate-fade-out-z', 'animate-fade-in-up');
         }.bind(this)
 }, '{ }');
