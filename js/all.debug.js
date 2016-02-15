9dff21b9f7c6550ec93c1da67be84c7164d405c7
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-02-16 00:11:53 +0100

    Update 2016-02-15T23:11:50.922Z

--- js/all.debug.js
+++ js/all.debug.js
@@ -2641,7 +2641,7 @@ riot.Tag = Tag
 
 //-----
 
-riot.tag2('card-list', '<div class="card-list"> <loader if="{!isDataLoaded}"></loader> <div if="{isDataLoaded}" class="card-list__card" each="{projects}" data-filter="{name.toLowerCase()} {url.toLowerCase()} {tags.toLowerCase()}"> <a href="{url}" class="card-list__image"> <img riot-src="{imgUrl}" alt="{imgAlt}"> </a> <h2 class="card-list__name"> {name} </h2> <div if="{url}" class="card-list__url" target="_blank" rel="nofollow"> {url.replace(/(http|https)\\:\\/\\/(www\\.)?/, \'\')} </div> <a if="{url}" href="#/details/{id}" class="card-list__details"> View Details </a> </div>', '', '', function(opts) {
+riot.tag2('card-list', '<div class="card-list"> <loader if="{!isDataLoaded}"></loader> <div if="{isDataLoaded}" class="card-list__card" each="{projects}" data-filter="{name.toLowerCase()} {url.toLowerCase()} {tags.toLowerCase()}"> <a href="{url}" target="_blank" rel="nofollow" class="card-list__image"> <img riot-src="{imgUrl}" alt="{imgAlt}"> </a> <h2 class="card-list__name"> {name} </h2> <div if="{url}" target="_blank" rel="nofollow" class="card-list__url"> {url.replace(/(http|https)\\:\\/\\/(www\\.)?/, \'\')} </div> <a if="{url}" href="#/details/{id}" class="card-list__details"> View Details </a> </div>', '', '', function(opts) {
         var _self=this;
         this.isDataLoaded=false;
         this.projects=[];
